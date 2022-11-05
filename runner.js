aa59c870c2340a8217cfd5b8f07b2d45fc9ae48b
joeyaronson <aronsonjoey@gmail.com>
2022-11-05 16:23:37 -0700

    update runner

--- runner.js
+++ runner.js
@@ -1,12 +1,28 @@
 
-const paths = ["boxes", "generative_cacti", "void", "fireflies", "graph", "wiggles"]
+const paths = [
+    "balls",
+    "boxes",
+    "cubes",
+    "drip",
+    "fireflies",
+    "flower",
+    "cacti",
+    "graph",
+    "grid",
+    "matrix",
+    "sin",
+    "tiedye",
+    "void",
+    "wave",
+    "wiggles"
+]
 setInterval(() => {
     let randomPath
-    do{
-    randomPath = ranChoice(paths)
-    }while(location.href.includes(randomPath))
+    do {
+        randomPath = ranChoice(paths)
+    } while (location.href.includes(randomPath))
     location.href = `../${randomPath}/index.html`
-}, 3600000);
+}, 200000);
 
 const ranChoice = (items) => items[Math.floor(Math.random() * items.length)]
 
