79447a52fc598dae39666c7c897547310db31efc
joeyaronson <aronsonjoey@gmail.com>
2022-11-12 14:12:31 -0800

    remove old homepage

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@ export DISPLAY=:0;
 if [ $# -eq 0 ]
   then
     echo "running home"
-    chromium-browser --kiosk --disable-infobar /home/pi/wall_display/index.html
+    chromium-browser --kiosk --disable-infobar /home/pi/wall_display/home.html
 else
     echo "running $1"
     chromium-browser --kiosk --disable-infobar /home/pi/wall_display/$1/index.html
