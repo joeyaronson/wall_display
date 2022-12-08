d984f35ef152f056c6b06fa950cc017b3acd2778
joeyaronson <aronsonjoey@gmail.com>
2022-12-07 23:13:54 -0800

    wave

--- wave/sketch.js
+++ wave/sketch.js
@@ -1,77 +1,156 @@
-setup = () => {
-	createCanvas(720, 1280, WEBGL);
+function setup() {
+	createCanvas(720, 1280);
 	rectMode(CENTER);
-	angleMode(DEGREES);
-	SIZE = width / 30;
-	loadTiles();
 	colorMode(HSB, 360);
+	angleMode(DEGREES);
+	loadBg();
+	setNoise();
+	loadSlices();
+	strokeWeight(2.5);
+	stroke(360, 0, 360, 250);
+}
+let s = [];
+let noiseVal;
+let SPACE = 20;
+let DETAIL = 75;
+let bg;
+let bg1;
+let bg2;
+let frameOff;
+
+let modes = ["rainbow", "bw", "rainbow2"];
+let mI = 2;
+let currMode;
+const loadBg = () => {
+	bg = random(360);
+	bg1 = color(bg, 80, 50);
+	bg2 = color(bg, 80, 100);
 };
-let t = [];
-let H = 10;
-let D = 200;
-let SIZE;
-let fc = 0;
+
+const setNoise = () => {
+	noiseVal = random(0.0008, 0.003);
+	frameOff = random(100, 200);
+	currMode = modes[mI % modes.length];
+};
 
 const INTERACTION_KEY = 53;
 function keyPressed() {
 	if (keyCode === INTERACTION_KEY) {
-		SIZE += 2
-		if (SIZE > 75) {
-			SIZE = width / 30
-		}
-		t = []
+		mI++;
+		loadBg();
+		setNoise();
+	}
+}
+function draw() {
+	background(220);
+	let gradient = drawingContext.createLinearGradient(0, height, 0, 0);
+	gradient.addColorStop(0, bg1.toString("#rrggbbaa"));
 
-		loadTiles();
+	gradient.addColorStop(1, bg2.toString("#rrggbbaa"));
+	drawingContext.fillStyle = gradient;
 
+	rect(width / 2, height / 2, width, height);
+
+	for (let i = 0; i < s.length; i++) {
+		s[i].calculateP();
+		s[i].display();
+	}
+
+	noiseVal += sin(frameCount + frameOff) / 800000;
+
+	if (frameCount % 20000 === 0) {
+		mI++;
+		loadBg();
+		setNoise();
 	}
 }
 
-draw = () => {
-	background(180);
-	rotateX(60);
-	rotateZ(fc / 2);
-
-	for (let i = 0; i < t.length; i++) {
-		t[i].move();
-		t[i].display();
+const loadSlices = () => {
+	for (let i = 15; i > 0; i--) {
+		s.push(
+			new Slice(width / 2 + 160 - i * SPACE, height / 2 + 200 - i * SPACE, i)
+		);
 	}
-	fc++;
 };
 
-class Tile {
-	constructor(x, y, z) {
+class Slice {
+	constructor(x, y, i) {
 		this.x = x;
 		this.y = y;
-		this.z = z;
+		this.i = i;
+		this.w = 400;
+		this.h = 150;
+		this.p = [];
 	}
+
+	calculateP() {
+		let p = [];
+		for (let i = 0; i < this.w; i += this.w / DETAIL) {
+			let n =
+				cos(
+					noise(
+						i * noiseVal,
+						(frameCount + frameOff - this.i * 20) * noiseVal
+					) * 1800
+				) * 100;
+			p.push({ x: i, y: n });
+		}
+		this.p = p;
+	}
+
 	display() {
-		stroke(0);
-		let c = map((this.d + fc) % 400, 0, 400, 0, 360);
-		fill((this.d + fc) % 360, 360, 360);
 		push();
-		let x = -width / 4 + this.x * SIZE;
-		let y = -width / 4 + this.y * SIZE;
 
-		translate(x, y, this.z);
-		rotateX(this.z + fc);
-		rotateY(this.z + fc);
-		box(SIZE, SIZE, SIZE / 5);
+		translate(this.x, this.y);
+		let gradient = drawingContext.createLinearGradient(0, 100, 0, -200);
 
+		if (currMode === "rainbow") {
+			for (let i = 0; i < 4; i++) {
+				let h = map(i, 0, 4, 0, 70);
+
+				let hex = color(
+					(h + frameCount + frameOff + this.i * 3) % 360,
+					360,
+					360 - this.i * 20,
+					220
+				).toString("#rrggbbaa");
+				gradient.addColorStop(i / 4, hex);
+			}
+			drawingContext.fillStyle = gradient;
+		} else if (currMode === "bw") {
+			gradient.addColorStop(1, "#000000AA");
+			gradient.addColorStop(0, "#ffffffAA");
+			drawingContext.fillStyle = gradient;
+		} else if (currMode === "rainbow2") {
+			let col1 = color(
+				(bg + 180 + (frameCount + frameOff) / 10) % 360,
+				360,
+				300 - this.i * 10,
+				220
+			);
+			let col2 = color(
+				(bg + 180 + 50 + (frameCount + frameOff) / 10) % 360,
+				360,
+				300 - this.i * 10,
+				220
+			);
+			gradient.addColorStop(0, col1.toString("#rrggbbaa"));
+			gradient.addColorStop(1, col2.toString("#rrggbbaa"));
+			drawingContext.fillStyle = gradient;
+		}
+
+		shearY(-30);
+
+		beginShape();
+
+		for (let { x, y } of this.p) {
+			vertex(-this.w / 2 + x, -this.h / 2 + y);
+		}
+		vertex(this.w / 2, this.h / 2);
+
+		vertex(-this.w / 2, this.h / 2);
+
+		endShape(CLOSE);
 		pop();
 	}
-	move() {
-		this.d = dist(0, 0, -width / 4 + this.x * SIZE, -width / 4 + this.y * SIZE);
-		this.z += sin(this.d + fc);
-	}
 }
-
-loadTiles = () => {
-	let count = width / 2 / SIZE;
-	for (let i = 0; i <= count; i++) {
-		for (let j = 0; j <= count; j++) {
-			t.push(new Tile(i, j, i));
-		}
-	}
-};
-
-pad = (a, b) => (1e15 + a + "").slice(-b);
