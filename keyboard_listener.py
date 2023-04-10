42d9ba1050bb7876f881b4b07c5c0a43793ce845
joeyaronson <aronsonjoey@gmail.com>
2023-04-09 22:41:52 -0700

    test

--- keyboard_listener.py
+++ keyboard_listener.py
@@ -4,6 +4,6 @@ import os
 import keyboard
   
 # press a to print rk
-keyboard.add_hotkey('ctrl + shift + a', lambda: os.system("echo test"))
+keyboard.add_hotkey('ctrl + shift + a', lambda: os.system("/home/joey/wall_display/start.sh"))
   
 keyboard.wait('esc')
