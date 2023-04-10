d302b446613d8cab938be6ac79b40a232fed6dcf
joeyaronson <aronsonjoey@gmail.com>
2023-04-09 19:33:25 -0700

    start

--- start.sh
+++ start.sh
@@ -4,8 +4,8 @@ if [ $# -eq 0 ]
   then
     echo "running home"
     kill_chrome
-    chromium-browser --kiosk --allow-file-access-from-files --disable-infobar /home/pi/wall_display/home.html
+    chromium-browser --kiosk --allow-file-access-from-files --disable-infobar ~/wall_display/home.html
 else
     echo "running $1"
-    chromium-browser --kiosk --disable-infobar /home/pi/wall_display/$1/index.html
+    chromium-browser --kiosk --disable-infobar ~/wall_display/$1/index.html
 fi
