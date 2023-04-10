7773312e507db8dcc568ab232620576b4847485d
joeyaronson <aronsonjoey@gmail.com>
2023-04-09 22:56:04 -0700

    test

--- keyboard_listener.py
+++ keyboard_listener.py
@@ -4,6 +4,6 @@ import os
 import keyboard
   
 # press a to print rk
-keyboard.add_hotkey('ctrl + shift + a', lambda: os.system("chromium-browser --kiosk --allow-file-access-from-files --disable-infobar ~/wall_display/home.html"))
+keyboard.add_hotkey('ctrl + shift + a', lambda: os.system("chromium-browser --kiosk --allow-file-access-from-files --disable-infobar ~/wall_display/home.html --no-sandbox"))
   
 keyboard.wait('esc')
