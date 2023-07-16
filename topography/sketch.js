17c1ed61d696a7e28d4b001f12515f6e1503d265
joeyaronson <aronsonjoey@gmail.com>
2023-07-16 00:20:58 -0700

    top test

--- topography/sketch.js
+++ topography/sketch.js
@@ -43,18 +43,21 @@ function setup() {
   function draw() {
 	background((frameCount / 10) % 100, 20, 100);
 	colorMode(RGB);
-	ambientLight(250, 250, 250);
-	// directionalLight(200, 200, 200, -1, 0.75, -1);
-	colorMode(HSB, 100);
+	// ambientLight(250, 250, 250);
   
 	rotateX(45);
-	rotateZ(frameCount / 4);
+	rotateZ(frameCount / 8);
+	// directionalLight(255, 255, 255, 1, 1, -1);
+	directionalLight(20, 20, 20, 1, -1, -1);
+	// directionalLight(255, 255, 255, -1, -1, -1);
+	colorMode(HSB, 100);
+  
 	for (let tile of t) {
 	  tile.display();
 	}
   }
-  let SIZE = 25;
-  let COUNT = 30;
+  let SIZE = 31;
+  let COUNT = 25;
   
   class Tile {
 	constructor(x, y, z, i) {
@@ -79,7 +82,7 @@ function setup() {
 	  if (h < 400) {
 		colOff = map(h, 0, 400, 100, 0);
   
-		fill(55, 100, 100 - colOff, 40);
+		emissiveMaterial(55, 100, 100 - colOff, 40);
 		push();
 		let oceanZ = h - 400;
 		let oceanOff = h / 2;
@@ -90,8 +93,8 @@ function setup() {
   
 	  //snow
 	  if (h >= 700) {
-		// fill(colors.snow);
-		fill(100);
+		// emissiveMaterial(colors.snow);
+		emissiveMaterial(100);
 		push();
 		let snowOff = h / 2;
 		translate(0, 0, snowOff + SIZE / 6);
@@ -101,15 +104,15 @@ function setup() {
   
 	  //tree
 	  if ((h > 525 && h < 575) || (h > 650 && h < 675)) {
-		// fill(colors.trunk);
-		fill(10, 100, 13);
+		// emissiveMaterial(colors.trunk);
+		emissiveMaterial(10, 100, 13);
 		push();
 		let treeOff = h / 2 + SIZE / 2;
 		translate(0, 0, treeOff);
 		box(this.w / 3, this.w / 3, SIZE);
   
-		// fill(colors.leaves);
-		fill(32, 100, 13);
+		// emissiveMaterial(colors.leaves);
+		emissiveMaterial(32, 100, 13);
 		translate(0, 0, SIZE / 1.5);
 		box(this.w / 2, this.w / 2, SIZE);
   
@@ -117,15 +120,15 @@ function setup() {
 	  }
 	  if (h < 500) {
 		colOff = map(h, 300, 500, 50, 0);
-		fill(10, 40, 69 - colOff);
+		emissiveMaterial(10, 40, 69 - colOff);
 	  } else if (h >= 500 && h < 600) {
 		colOff = map(h, 500, 600, 0, 30);
   
-		fill(40, 51, 57 - colOff);
+		emissiveMaterial(40, 51, 57 - colOff);
 	  } else if (h >= 600) {
 		colOff = map(h, 600, 800, 30, 0);
   
-		fill(40, 4, 50 - colOff);
+		emissiveMaterial(40, 4, 50 - colOff);
 	  }
   
 	  box(this.w, this.w, h);
